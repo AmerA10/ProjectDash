5730de946a09674983a2f3f735daa8be4e3788a7
AmerA10 <ajami.amer1@gmail.com>
2022-02-12 21:32:56 -0700

    changed some settings

--- Assets/Scripts/Dashables/DefaultDash.cs
+++ Assets/Scripts/Dashables/DefaultDash.cs
@@ -6,7 +6,7 @@ public class DefaultDash : Dashable
 {
     // Start is called before the first frame update
 
-    [SerializeField] float dashSpeed = 85f;
+    [SerializeField] float dashSpeed = 100f;
 
     private void Awake()
     {
